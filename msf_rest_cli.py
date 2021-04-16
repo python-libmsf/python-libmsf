92e38ae954b1ea5e5fb37ac42112f75929478ea8
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2021-04-16 14:53:20 +0300

    Add msf_rest_cli.py Add console client for Metasploit REST API library

---
+++ msf_rest_cli.py
@@ -0,0 +1,25 @@
+# Description
+"""
+msf_rest_cli.py: MSF REST API console client
+Author: Vladimir Ivanov
+License: MIT
+Copyright 2021, Python Metasploit Library
+"""
+
+# Import
+from libmsf.rest import main
+
+# Authorship information
+__author__ = 'Vladimir Ivanov'
+__copyright__ = 'Copyright 2021, Python Metasploit Library'
+__credits__ = ['']
+__license__ = 'GNU AGPLv3'
+__version__ = '0.0.1'
+__maintainer__ = 'Vladimir Ivanov'
+__email__ = 'ivanov.vladimir.mail@gmail.com'
+__status__ = 'Development'
+
+
+# Call Main function
+if __name__ == "__main__":
+    main()
