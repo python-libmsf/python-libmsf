d70c2c83f47d3044fccd65d42453c0cfb0b7280b
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2021-04-18 20:25:09 +0300

    Add msf_parser_cli.py Add console client for parse MSF exported file

---
+++ msf_parser_cli.py
@@ -0,0 +1,25 @@
+# Description
+"""
+msf_parser_cli.py: Console client for parse MSF exported files
+Author: Vladimir Ivanov
+License: MIT
+Copyright 2021, Python Metasploit Library
+"""
+
+# Import
+from libmsf.parser import main
+
+# Authorship information
+__author__ = 'Vladimir Ivanov'
+__copyright__ = 'Copyright 2021, Python Metasploit Library'
+__credits__ = ['']
+__license__ = 'MIT'
+__version__ = '0.0.5'
+__maintainer__ = 'Vladimir Ivanov'
+__email__ = 'ivanov.vladimir.mail@gmail.com'
+__status__ = 'Development'
+
+
+# Call Main function
+if __name__ == "__main__":
+    main()
